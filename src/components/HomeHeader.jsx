27237b1a4305bcd8227596a2b9251e51ea0ff000
Nour El Turky <nourelturky9@gmail.com>
2023-07-14 23:31:51 +0300

    add pagination component

--- src/components/HomeHeader.jsx
+++ src/components/HomeHeader.jsx
@@ -1,24 +1,30 @@
 import Link from "next/link";
-import {TbGridDots} from 'react-icons/tb'
+import { TbGridDots } from "react-icons/tb";
 export default function HomeHeader() {
-return (
-<header className=" flex justify-end p-5 ">
-<div className="flex space-x-4 items-center">
-<Link href='https://mail.google.com'
-className="hover:underline text-1xl"
->
-Gmail
-</Link>
+  return (
+    <header className=" flex justify-end p-5 ">
+      <div className="flex space-x-4 items-center">
+        <Link
+          href="https://mail.google.com"
+          className="hover:underline text-1xl"
+        >
+          Gmail
+        </Link>
 
-<Link href='https://image.google.com'
-className="hover:underline text-1xl"
->
-Images 
-</Link>
-<TbGridDots className="bg-transparent hover:bg-gray-100  rounded-full text-4xl p-2"/>
-<button className="bg-blue-600 text-white px-6 py-2 font-medium
-rounded-md hover:brightness-105 hover:shadow-md transition-shadow">Sigh in</button>
-</div>
-</header>
-    )
+        <Link
+          href="https://image.google.com"
+          className="hover:underline text-1xl"
+        >
+          Images
+        </Link>
+        <TbGridDots className="bg-transparent hover:bg-gray-100  rounded-full text-4xl p-2" />
+        <button
+          className="bg-blue-600 text-white px-6 py-2 font-medium
+rounded-md hover:brightness-105 hover:shadow-md transition-shadow"
+        >
+          Sign in
+        </button>
+      </div>
+    </header>
+  );
 }
